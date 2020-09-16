17485cd52308d5d2b8403451572c4177d3af0470
DilshanKumarasinghe <dilshankumarasinghe@gmail.com>
2020-09-16 12:14:37 +0530

    map bugg fix

--- NIBM COVID19/Controllers/MapViewController.swift
+++ NIBM COVID19/Controllers/MapViewController.swift
@@ -18,12 +18,12 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
        configureUI()
-       enableLocationServices()
+        enableLocationServices()
        
     }
     
    // MARK: - Properties
-   private let inputActivationUIView = LocationInputActivationUIView ()
+ 
     private let mapView = MKMapView()
     private let locationManager = CLLocationManager()
 
@@ -47,34 +47,17 @@ class MapViewController: UIViewController {
     //MARK: - Helper Functions
 
     func configureUI() {
-        
-        
-    inputActivationUIView.alpha = 0
+ 
     confugireMapView()
-        
-        
-    UIView.animate(withDuration: 2) {
-        self.inputActivationUIView.alpha = 1
-    }      
-   
-        
-   view.addSubview(inputActivationUIView)
-   inputActivationUIView.centerX(inView: view)
-   inputActivationUIView.setDimensions(height: 50, width: view.frame.width - 64)
-   inputActivationUIView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
-
-        
-        
-
-    view.addSubview(mapView)
+     view.addSubview(mapView)
     mapView.frame = view.frame
 
 //Back Button -...........
-view.addSubview(closeButton)
-closeButton.translatesAutoresizingMaskIntoConstraints = false
-closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-closeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
-    }
+    view.addSubview(closeButton)
+    closeButton.translatesAutoresizingMaskIntoConstraints = false
+    closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+    closeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        }
 
 func confugireMapView() {
        view.addSubview(mapView)
@@ -85,49 +68,36 @@ func confugireMapView() {
    }
     
     // MARK: - Selectors
-    
-    @objc func handleShowLocationInputView() {
-        
-    }
-
+ 
 
     
 }
 
 //  MARK: - LocationServices
    extension MapViewController: CLLocationManagerDelegate {
-
-        func enableLocationServices() {
-
-            locationManager.delegate = self
-
-            switch CLLocationManager.authorizationStatus() {
-            case .notDetermined:
-                locationManager.requestWhenInUseAuthorization()
-            case .restricted, .denied:
-                break
-            case .authorizedWhenInUse:
-                locationManager.requestAlwaysAuthorization()
-            case .authorizedAlways:
-                locationManager.startUpdatingLocation()
-                locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            default:
-                break
-            }
-        }
-
-
+       
+       func enableLocationServices() {
+           
+           locationManager.delegate = self
+           
+           switch CLLocationManager.authorizationStatus() {
+           case .notDetermined:
+               locationManager.requestWhenInUseAuthorization()
+           case .restricted, .denied:
+               break
+           case .authorizedWhenInUse:
+               locationManager.requestAlwaysAuthorization()
+           case .authorizedAlways:
+               locationManager.startUpdatingLocation()
+               locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           default:
+               break
+           }
+       }
+       
        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-             if status == .authorizedWhenInUse {
-                 locationManager.requestAlwaysAuthorization()
-             }
-         }
-
-}
-extension MapViewController: LocationInputActivationUIViewDelegate {
-    func presentLocationInputView() {
-        
-        inputActivationUIView.delegate = self
-        print("DEBUG: LocationInputActivationUIViewDelegate called")
-    }
-}
+           if status == .authorizedWhenInUse {
+               locationManager.requestAlwaysAuthorization()
+           }
+       }
+   }
